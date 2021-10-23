5ce824c09b74d6e941d26f70bc13c877b5056ad3
Sisnando Junior <nando.devs@gmail.com>
2021-10-23 15:56:46 -0300

    Update person by id

--- src/main/java/com/nandodevs/personsapi/service/PersonService.java
+++ src/main/java/com/nandodevs/personsapi/service/PersonService.java
@@ -32,10 +32,7 @@ public class PersonService {
         Person personToSave = personMapper.toModel(personDTO);
 
         Person savedPerson = personRepository.save(personToSave);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId()) //Messagem ao salvar dados
-                .build();
+        return createMessageResponse(savedPerson.getId(), "Created person with ID ");
     }
 
     public List<PersonDTO> listAll() {
@@ -59,13 +56,24 @@ public class PersonService {
         personRepository.deleteById(id);
     }
 
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+
+        Person persontoUpdate = personMapper.toModel(personDTO);
+
+        Person updatedPerson = personRepository.save(persontoUpdate);
+        return createMessageResponse(updatedPerson.getId(), "Updated person with ID ");
+    }
+
     private Person verifyIfExists(Long id) throws PersonNotFoundException {
         return personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
     }
 
-
-
-
-
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id) //Messagem ao salvar dados
+                .build();
+    }
 }
