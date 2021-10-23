63e3a74aeb3614d1d73503bbe1b380225d942d8a
Sisnando Junior <nando.devs@gmail.com>
2021-10-23 18:40:15 -0300

    Index page configuration

--- src/main/java/com/nandodevs/personsapi/controller/IndexController.java
+++ src/main/java/com/nandodevs/personsapi/controller/IndexController.java
@@ -1,18 +1,18 @@
 package com.nandodevs.personsapi.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class IndexController {
 
-    @Controller
-    @RequestMapping(value = "/")
-    public class PageController {
+    @GetMapping("/home")
+    public ModelAndView Index(){
 
-        @RequestMapping(method = RequestMethod.GET)
-        public String getIndexPage() {
-            return "index.html";
-        }
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        modelAndView.getView();
+        return modelAndView;
     }
 }
